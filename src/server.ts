d5113aa4ee9fe3527e3080f8601c77aae27e95a1
César Oliveira <cesar.oliveira@takelog.com.br>
2021-02-24 00:43:11 -0300

    conexão com banco de dados, criação de migration, modal, controller e rotas de usuários

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,13 @@
+import 'reflect-metadata';
 /*Importando express*/
 import express from 'express';
+import "./database";
+import { router } from './routes';
 
-/*Chamando o express*/
 const app = express();
 
-/*
-GET - Buscar
-POST - Salvar
-PUT - Alterar
-DELETE - Delear 
-PATCH - Alteração expecifica
-*/
-
-/*Criando uma rota get, enviado uma mensagem para o cliente*/
-app.get('/', (req, res) => {
-    res.send('Olá Mundo!')
-});
-
-/*Criando um rota post*/
-app.post('/', (req, res) => {
-    //Recebemos os dados para salvar
-    res.json({'message':'Dados Salvos com Sucesso'})
-});
-
+app.use(express.json());
+app.use(router);
 
 
 app.listen(3333, () => console.log('Server in Running'));
